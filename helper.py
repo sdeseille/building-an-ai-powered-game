a08c0ce93247d9a85a90792c29f4a87b808f5c79
sdeseille <sebastien.deseille@gmail.com>
2024-12-24 15:23:14 +0100

    add contents related to chapter 2

--- helper.py
+++ helper.py
@@ -2,19 +2,21 @@
 
 import os
 from dotenv import load_dotenv, find_dotenv
+import json
+from together import Together
 
 # these expect to find a .env file at the directory above the lesson.                                                         # the format for that file is (without the comment)                                                                           # API_KEYNAME=AStringThatIsTheLongAPIKeyFromSomeService                                                                                                                                     
 def load_env():
     _ = load_dotenv(find_dotenv())
 
-def save_world(world, filename):
-    with open(filename, 'w') as f:
-        json.dump(world, f)
-
 def load_world(filename):
     with open(filename, 'r') as f:
         return json.load(f)
 
+def save_world(world, filename):
+    with open(filename, 'w') as f:
+        json.dump(world, f)
+        
 def get_together_api_key():
      load_env()
      together_api_key = os.getenv("TOGETHER_API_KEY")
